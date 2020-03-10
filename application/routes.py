816b82bf651a883444f34350ab1e047f3aa16cf0
Marbul83 <markbulfin@gmail.com>
2020-03-10 16:45:29 +0000

    flask-blog day one, database working

--- application/routes.py
+++ application/routes.py
@@ -1,21 +1,25 @@
-from flask import render_template
-from application import app
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
+
+#blogData = [
+   # {  
+        #"name": {"first":"John", "last":"Doe"},
+     #   "title":"First Post",
+    #    "content":"This is some blog data for Flask lectures"
+   # },
+   # {   
+    #    "name": {"first":"Jane", "last":"Doe"},
+   #     "title":"Second Post",
+   #     "content":"This is even more blog data for Flask lectures"
+  #  }
+#]
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home', blogData=blogData)
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
@@ -28,3 +32,25 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title='Register Page')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+
